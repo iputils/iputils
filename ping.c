ba6b64599e57905e5e432f82a782f1582f7f43d1
YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
2006-04-16 19:45:16 +0900

    [PING,PING6] Avoid using __constant_htons() if it is really needed
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

--- ping.c
+++ ping.c
@@ -1187,7 +1187,7 @@ void install_filter(void)
 	once = 1;
 
 	/* Patch bpflet for current identifier. */
-	insns[2] = (struct sock_filter)BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __constant_htons(ident), 0, 1);
+	insns[2] = (struct sock_filter)BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, htons(ident), 0, 1);
 
 	if (setsockopt(icmp_sock, SOL_SOCKET, SO_ATTACH_FILTER, &filter, sizeof(filter)))
 		perror("WARNING: failed to install socket filter\n");
