d2ae921f6859725f96672ac437b48b20cd7483f3
YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
2006-09-27 15:22:54 +0900

    From: Mitsuru Chinen <mitch@jp.ibm.com>
    Fix compilation errors about labels at end of compound statements.
    
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

--- clockdiff.c
+++ clockdiff.c
@@ -210,7 +210,7 @@ empty:
 
 			if ((count = select(FD_SETSIZE, &ready, (fd_set *)0,
 			    (fd_set *)0, &tout)) <= 0)
-				goto send_next;
+				break;
 
 			(void)gettimeofday(&tv1, (struct timezone *)0);
 			cc = recvfrom(sock_raw, (char *)packet, PACKET_IN, 0, 
@@ -287,7 +287,6 @@ empty:
         		}
                       }
 		}
-send_next: ;
 	}
 
 good_exit:
@@ -392,7 +391,7 @@ empty:
 
 			if ((count = select(FD_SETSIZE, &ready, (fd_set *)0,
 			    (fd_set *)0, &tout)) <= 0)
-				goto send_next;
+				break;
 
 			(void)gettimeofday(&tv1, (struct timezone *)0);
 			cc = recvfrom(sock_raw, (char *)packet, PACKET_IN, 0, 
@@ -495,7 +494,6 @@ empty:
 				}
 			}
 		}
-send_next: ;
 	}
 
 good_exit:
