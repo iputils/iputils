70bcae855fc2fd6cdded0ee9c650aebcd6379515
YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
2012-10-10 17:56:42 +0900

    ninfod: Fix off-by-one error to check possible programming error.
    This bug is not fatal at all because this check is only for
    possible programming error (which is not the case so far).
    
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

--- ninfod/ninfod_core.c
+++ ninfod/ninfod_core.c
@@ -316,7 +316,8 @@ void init_nodeinfo_suptypes(INIT_ARGS)
 		qtype = qtypeinfo_table[i].qtype;
 		w = qtype>>5;
 		b = qtype&0x1f;
-		if (w > sizeof(suptypes)/sizeof(suptypes[0])) {
+		if (w >= sizeof(suptypes)/sizeof(suptypes[0])) {
+			/* This is programming error. */
 			DEBUG(LOG_ERR, "Warning: Too Large Supported Types\n");
 			exit(1);
 		}
