iputils/iputils

commit 8feb586c4c6df32badb159947ed233898891aecd

ping,ping6: Check return value of write(2) for stdout.
This fixes following warnings.

| ping.c: In function ‘parse_reply’:
| ping.c:792:12: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]
| ping.c:810:10: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]
| ping.c:812:10: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]
| ping.c: In function ‘receive_error_msg’:
| ping.c:583:9: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]
| ping.c:619:9: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]

| ping6.c: In function ‘parse_reply’:
| ping6.c:1435:10: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]
| ping6.c: In function ‘receive_error_msg’:
| ping6.c:1074:9: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]
| ping6.c:1097:9: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]

| ping_common.c: In function ‘gather_statistics’:
| ping_common.c:768:9: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]
| ping_common.c:770:9: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]
| ping_common.c: In function ‘pinger’:
| ping_common.c:374:10: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]
| ping_common.c:427:9: warning: ignoring return value of ‘write’, declared with attribute warn_unused_result [-Wunused-result]

Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

ping_common.h
@@ -130,6 +130,18 @@ case 'D': case 'O':
 
 #define COMMON_OPTSTR "h?VQ:I:M:aUc:dfi:w:l:S:np:qrs:vLt:AW:Bm:DO"
 
+/*
+ * Write to stdout
+ */
+static inline void write_stdout(const char *str, size_t len)
+{
+	size_t o = 0;
+	ssize_t cc;
+	do {
+		cc = write(STDOUT_FILENO, str + o, len - o);
+		o += cc;
+	} while (len >= o || cc < 0);
+}
 
 /*
  * tvsub --

ping.c
@@ -580,7 +580,7 @@ int receive_error_msg()
 		if (options & F_QUIET)
 			goto out;
 		if (options & F_FLOOD)
-			write(STDOUT_FILENO, "E", 1);
+			write_stdout("E", 1);
 		else if (e->ee_errno != EMSGSIZE)
 			fprintf(stderr, "ping: local error: %s\n", strerror(e->ee_errno));
 		else
@@ -616,7 +616,7 @@ int receive_error_msg()
 		if (options & F_QUIET)
 			goto out;
 		if (options & F_FLOOD) {
-			write(STDOUT_FILENO, "\bE", 2);
+			write_stdout("\bE", 2);
 		} else {
 			print_timestamp();
 			printf("From %s icmp_seq=%u ", pr_addr(sin->sin_addr.s_addr), ntohs(icmph.un.echo.sequence));
@@ -789,7 +789,7 @@ parse_reply(struct msghdr *msg, int cc, void *addr, struct timeval *tv)
 					return !error_pkt;
 				if (options & F_FLOOD) {
 					if (error_pkt)
-						write(STDOUT_FILENO, "\bE", 2);
+						write_stdout("\bE", 2);
 					return !error_pkt;
 				}
 				print_timestamp();
@@ -807,9 +807,9 @@ parse_reply(struct msghdr *msg, int cc, void *addr, struct timeval *tv)
 		}
 		if ((options & F_FLOOD) && !(options & (F_VERBOSE|F_QUIET))) {
 			if (!csfailed)
-				write(STDOUT_FILENO, "!E", 2);
+				write_stdout("!E", 2);
 			else
-				write(STDOUT_FILENO, "!EC", 3);
+				write_stdout("!EC", 3);
 			return 0;
 		}
 		if (!(options & F_VERBOSE) || uid)

ping6.c
@@ -1071,7 +1071,7 @@ int receive_error_msg()
 		if (options & F_QUIET)
 			goto out;
 		if (options & F_FLOOD)
-			write(STDOUT_FILENO, "E", 1);
+			write_stdout("E", 1);
 		else if (e->ee_errno != EMSGSIZE)
 			fprintf(stderr, "ping: local error: %s\n", strerror(e->ee_errno));
 		else
@@ -1094,7 +1094,7 @@ int receive_error_msg()
 		if (options & F_QUIET)
 			goto out;
 		if (options & F_FLOOD) {
-			write(STDOUT_FILENO, "\bE", 2);
+			write_stdout("\bE", 2);
 		} else {
 			print_timestamp();
 			printf("From %s icmp_seq=%u ", pr_addr(&sin6->sin6_addr), ntohs(icmph.icmp6_seq));
@@ -1432,7 +1432,7 @@ parse_reply(struct msghdr *msg, int cc, void *addr, struct timeval *tv)
 				return 0;
 			nerrors++;
 			if (options & F_FLOOD) {
-				write(STDOUT_FILENO, "\bE", 2);
+				write_stdout("\bE", 2);
 				return 0;
 			}
 			print_timestamp();

ping_common.c
@@ -371,7 +371,7 @@ resend:
 			 * high preload or pipe size is very confusing. */
 			if ((preload < screen_width && pipesize < screen_width) ||
 			    in_flight() < screen_width)
-				write(STDOUT_FILENO, ".", 1);
+				write_stdout(".", 1);
 		}
 		return interval - tokens;
 	}
@@ -424,7 +424,7 @@ resend:
 
 	if (i == 0 && !(options & F_QUIET)) {
 		if (options & F_FLOOD)
-			write(STDOUT_FILENO, "E", 1);
+			write_stdout("E", 1);
 		else
 			perror("ping: sendmsg");
 	}
@@ -765,9 +765,9 @@ restamp:
 
 	if (options & F_FLOOD) {
 		if (!csfailed)
-			write(STDOUT_FILENO, "\b \b", 3);
+			write_stdout("\b \b", 3);
 		else
-			write(STDOUT_FILENO, "\bC", 1);
+			write_stdout("\bC", 1);
 	} else {
 		int i;
 		__u8 *cp, *dp;

commit 712fddacfb5c4a8a48d9c5debe1870bc051d836c

ping,ping6: Fix hang with -f option.
Bug was introduced by commit 8feb586c4... (ping,ping6: Check return
value of write(2) for stdout.).

https://bugs.archlinux.org/task/32306

Patch-by: Jan Synacek <jsynacek@redhat.com>
Reported-by: Mohammad Alsaleh <msal@tormail.org>
Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

ping_common.h
@@ -140,7 +140,7 @@ static inline void write_stdout(const char *str, size_t len)
 	do {
 		cc = write(STDOUT_FILENO, str + o, len - o);
 		o += cc;
-	} while (len >= o || cc < 0);
+	} while (len > o || cc < 0);
 }
 
 /*