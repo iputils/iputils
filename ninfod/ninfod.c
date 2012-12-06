a12f8d430e77ac8279d0f55bc81dc9cc2da2c70b
YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
2012-12-07 01:29:55 +0900

    ninfod: nanosleep(3) needs <time.h>.
    | ninfod.c: In function 'ni_send':
    | ninfod.c:363:4: warning: implicit declaration of function 'nanosleep' [-Wimplicit-function-declaration]
    
    Patch based on iputils-20121125-ninfod-minor.patch from Fedora.
    
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

--- ninfod/ninfod.c
+++ ninfod/ninfod.c
@@ -73,6 +73,17 @@
 # include <unistd.h>
 #endif
 
+#ifdef TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# ifdef HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
 #if HAVE_SYS_UIO_H
 #include <sys/uio.h>
 #endif
