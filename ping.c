iputils/iputils

commit 63579a1b5bf61f0e03dd4e815386170d684797f7

ping: Warn if kernel has selected source address from other interface.
Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

ping.c
@@ -62,6 +62,9 @@ char copyright[] =
 
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
+#ifndef WITHOUT_IFADDRS
+#include <ifaddrs.h>
+#endif
 
 #ifndef ICMP_FILTER
 #define ICMP_FILTER	1
@@ -309,6 +312,10 @@ main(int argc, char **argv)
 		socklen_t alen;
 		struct sockaddr_in dst = whereto;
 		int probe_fd = socket(AF_INET, SOCK_DGRAM, 0);
+#ifndef WITHOUT_IFADDRS
+		struct ifaddrs *ifa0, *ifa;
+		int ret;
+#endif
 
 		if (probe_fd < 0) {
 			perror("socket");
@@ -379,6 +386,25 @@ main(int argc, char **argv)
 			exit(2);
 		}
 		source.sin_port = 0;
+
+#ifndef WITHOUT_IFADDRS
+		ret = getifaddrs(&ifa0);
+		if (ret) {
+			fprintf(stderr, "gatifaddrs() failed.\n");
+			exit(2);
+		}
+		for (ifa = ifa0; ifa; ifa = ifa->ifa_next) {
+			if (!ifa->ifa_addr || ifa->ifa_addr->sa_family != AF_INET)
+				continue;
+			if (!strncmp(ifa->ifa_name, device, sizeof(device) - 1) &&
+			    !memcmp(&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr,
+				    &source.sin_addr, sizeof(source.sin_addr)))
+				break;
+		}
+		freeifaddrs(ifa0);
+		if (!ifa)
+			fprintf(stderr, "ping: Warning: source address might be selected on device other than %s.\n", device);
+#endif
 		close(probe_fd);
 	} while (0);
 

ping6.c
@@ -72,6 +72,9 @@ char copyright[] =
 #include <netinet/ip6.h>
 #include <netinet/icmp6.h>
 #include <resolv.h>
+#ifndef WITHOUT_IFADDRS
+#include <ifaddrs.h>
+#endif
 
 #ifdef USE_IDN
 #include <stringprep.h>
@@ -937,6 +940,9 @@ int main(int argc, char *argv[])
 	if (IN6_IS_ADDR_UNSPECIFIED(&source.sin6_addr)) {
 		socklen_t alen;
 		int probe_fd = socket(AF_INET6, SOCK_DGRAM, 0);
+#ifndef WITHOUT_IFADDRS
+		struct ifaddrs *ifa0, *ifa;
+#endif
 
 		if (probe_fd < 0) {
 			perror("socket");
@@ -981,6 +987,26 @@ int main(int argc, char *argv[])
 		}
 		source.sin6_port = 0;
 		close(probe_fd);
+
+#ifndef WITHOUT_IFADDRS
+		if (getifaddrs(&ifa0)) {
+			perror("getifaddrs");
+			exit(2);
+		}
+
+		for (ifa = ifa0; ifa; ifa = ifa->ifa_next) {
+			if (!ifa->ifa_addr || ifa->ifa_addr->sa_family != AF_INET6)
+				continue;
+			if (!strncmp(ifa->ifa_name, device, sizeof(device) - 1) &&
+			    IN6_ARE_ADDR_EQUAL(&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr,
+					       &source.sin6_addr))
+				break;
+		}
+		if (!ifa)
+			fprintf(stderr, "ping6: Warning: source address might be selected on device other than %s.\n", device);
+
+		freeifaddrs(ifa0);
+#endif
 	}
 #ifdef HAVE_SIN6_SCOPEID
 	else if (device && (IN6_IS_ADDR_LINKLOCAL(&source.sin6_addr) ||

Makefile
@@ -138,9 +138,9 @@ LIB_clockdiff = $(LIB_CAP)
 
 # ping / ping6
 DEF_ping_common = $(DEF_CAP) $(DEF_IDN)
-DEF_ping  = $(DEF_CAP) $(DEF_IDN)
+DEF_ping  = $(DEF_CAP) $(DEF_IDN) $(DEF_WITHOUT_IFADDRS)
 LIB_ping  = $(LIB_CAP) $(LIB_IDN)
-DEF_ping6 = $(DEF_CAP) $(DEF_IDN) $(DEF_ENABLE_PING6_RTHDR)
+DEF_ping6 = $(DEF_CAP) $(DEF_IDN) $(DEF_WITHOUT_IFADDRS) $(DEF_ENABLE_PING6_RTHDR)
 LIB_ping6 = $(LIB_CAP) $(LIB_IDN) $(LIB_RESOLV) $(LIB_CRYPTO)
 
 ping: ping_common.o

commit 961cac1deeb1325d45f46dbbe20a922fa08746c1

ping,ping6: Check outgoing device only if specified.
This fixes SEGV when ping'ing to address without -I option.
This bug was introduced by commit 63579a1b ("ping: Warn if kernel has
selected source address from other interface.").

Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

ping.c
@@ -312,10 +312,6 @@ main(int argc, char **argv)
 		socklen_t alen;
 		struct sockaddr_in dst = whereto;
 		int probe_fd = socket(AF_INET, SOCK_DGRAM, 0);
-#ifndef WITHOUT_IFADDRS
-		struct ifaddrs *ifa0, *ifa;
-		int ret;
-#endif
 
 		if (probe_fd < 0) {
 			perror("socket");
@@ -388,22 +384,27 @@ main(int argc, char **argv)
 		source.sin_port = 0;
 
 #ifndef WITHOUT_IFADDRS
-		ret = getifaddrs(&ifa0);
-		if (ret) {
-			fprintf(stderr, "gatifaddrs() failed.\n");
-			exit(2);
+		if (device) {
+			struct ifaddrs *ifa0, *ifa;
+			int ret;
+
+			ret = getifaddrs(&ifa0);
+			if (ret) {
+				fprintf(stderr, "gatifaddrs() failed.\n");
+				exit(2);
+			}
+			for (ifa = ifa0; ifa; ifa = ifa->ifa_next) {
+				if (!ifa->ifa_addr || ifa->ifa_addr->sa_family != AF_INET)
+					continue;
+				if (!strncmp(ifa->ifa_name, device, sizeof(device) - 1) &&
+				    !memcmp(&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr,
+					    &source.sin_addr, sizeof(source.sin_addr)))
+					break;
+			}
+			freeifaddrs(ifa0);
+			if (!ifa)
+				fprintf(stderr, "ping: Warning: source address might be selected on device other than %s.\n", device);
 		}
-		for (ifa = ifa0; ifa; ifa = ifa->ifa_next) {
-			if (!ifa->ifa_addr || ifa->ifa_addr->sa_family != AF_INET)
-				continue;
-			if (!strncmp(ifa->ifa_name, device, sizeof(device) - 1) &&
-			    !memcmp(&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr,
-				    &source.sin_addr, sizeof(source.sin_addr)))
-				break;
-		}
-		freeifaddrs(ifa0);
-		if (!ifa)
-			fprintf(stderr, "ping: Warning: source address might be selected on device other than %s.\n", device);
 #endif
 		close(probe_fd);
 	} while (0);

ping6.c
@@ -940,9 +940,6 @@ int main(int argc, char *argv[])
 	if (IN6_IS_ADDR_UNSPECIFIED(&source.sin6_addr)) {
 		socklen_t alen;
 		int probe_fd = socket(AF_INET6, SOCK_DGRAM, 0);
-#ifndef WITHOUT_IFADDRS
-		struct ifaddrs *ifa0, *ifa;
-#endif
 
 		if (probe_fd < 0) {
 			perror("socket");
@@ -989,23 +986,27 @@ int main(int argc, char *argv[])
 		close(probe_fd);
 
 #ifndef WITHOUT_IFADDRS
-		if (getifaddrs(&ifa0)) {
-			perror("getifaddrs");
-			exit(2);
-		}
+		if (device) {
+			struct ifaddrs *ifa0, *ifa;
 
-		for (ifa = ifa0; ifa; ifa = ifa->ifa_next) {
-			if (!ifa->ifa_addr || ifa->ifa_addr->sa_family != AF_INET6)
-				continue;
-			if (!strncmp(ifa->ifa_name, device, sizeof(device) - 1) &&
-			    IN6_ARE_ADDR_EQUAL(&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr,
-					       &source.sin6_addr))
-				break;
-		}
-		if (!ifa)
-			fprintf(stderr, "ping6: Warning: source address might be selected on device other than %s.\n", device);
+			if (getifaddrs(&ifa0)) {
+				perror("getifaddrs");
+				exit(2);
+			}
 
-		freeifaddrs(ifa0);
+			for (ifa = ifa0; ifa; ifa = ifa->ifa_next) {
+				if (!ifa->ifa_addr || ifa->ifa_addr->sa_family != AF_INET6)
+					continue;
+				if (!strncmp(ifa->ifa_name, device, sizeof(device) - 1) &&
+				    IN6_ARE_ADDR_EQUAL(&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr,
+						       &source.sin6_addr))
+					break;
+			}
+			if (!ifa)
+				fprintf(stderr, "ping6: Warning: source address might be selected on device other than %s.\n", device);
+
+			freeifaddrs(ifa0);
+		}
 #endif
 	}
 #ifdef HAVE_SIN6_SCOPEID