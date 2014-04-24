iputils/iputils

commit c9ecbd72ff6787354111a47985b1bee5d699e39d

fix ifdef to apply only on uclibc and old glibc

rdisc.c
@@ -115,7 +115,7 @@ static void init(void);
 
 #define MAXIFS 32
 
-#if !defined(__GLIBC__) || __GLIBC__ < 2
+#if defined(__UCLIBC__) || (defined(__GLIBC__) && __GLIBC__ < 2)
 /* For router advertisement */
 struct icmp_ra
 {

commit cf8fa7aafa2a0fd6c99dfda1984a4e02a24bdebd

rdisc: ifdef only apply on historic glibc < 2
This fix compilation on uclibc accidentally broken by
commit c9ecbd72ff6787354111a47985b1bee5d699e39d.
Tested on uClibc 0.9.33.2 with 3.4 headers.

Signed-off-by: David Heidelberger <david.heidelberger@ixit.cz>
Tested-by: Gustavo Zacarias <gustavo@zacarias.com.ar>

rdisc.c
@@ -115,7 +115,7 @@ static void init(void);
 
 #define MAXIFS 32
 
-#if defined(__UCLIBC__) || (defined(__GLIBC__) && __GLIBC__ < 2)
+#if defined(__GLIBC__) && __GLIBC__ < 2
 /* For router advertisement */
 struct icmp_ra
 {