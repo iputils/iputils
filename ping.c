iputils/iputils

commit 59de58d0f92a2c9b90174e51070aca32e7959e28

iputils-s20100214

ping.c
@@ -805,6 +805,9 @@ parse_reply(struct msghdr *msg, int cc, void *addr, struct timeval *tv)
 			putchar('\a');
 		putchar('\n');
 		fflush(stdout);
+	} else {
+		putchar('\a');
+		fflush(stdout);
 	}
 	return 0;
 }

ping6.c
@@ -880,6 +880,9 @@ parse_reply(struct msghdr *msg, int cc, void *addr, struct timeval *tv)
 			putchar('\a');
 		putchar('\n');
 		fflush(stdout);
+	} else {
+		putchar('\a');
+		fflush(stdout);
 	}
 	return 0;
 }

RELNOTES
@@ -1,3 +1,26 @@
+[s20100214]
+
+Jamal Hadi Salim (2):
+      ping: ping by mark
+      ping: ping by mark doc update
+
+Jamie Le Tual (1):
+      ping: set un.echo.id to network byte order
+
+YOSHIFUJI Hideaki (11):
+      [PING6,TRACEROUTE6]: Ignore error in setting IPV6_CHECKSUM socket option for ICMPv6 socket.
+      [PING6]: Use if_nametoindex() to convert ifname to ifindex.
+      [PING6]: Allow to specify source address with interface in a single -I option.
+      ping6: Try using IPV6_PKTINFO sticky option to specify outgoing interface.
+      rdisc: Use FOPEN_MAX if OPEN_MAX is undefined.
+      ping6: Fix source routing with source interface set.
+      ping,ping6: Don't print extra ', ' in finish().
+      tracepath: Fix documentation typo.
+      Use sysconf(_SC_OPEN_MAX) instead of OPEN_MAX.
+      ping,ping6: Add -D option to print timestamp.
+      Merge branch 'timestamp'
+
+
 [s20071127]
 
 John Heffner (6):

SNAPSHOT.h
@@ -1,1 +1,1 @@
-static char SNAPSHOT[] = "s20071127";
+static char SNAPSHOT[] = "s20100214";

doc/snapshot.db
@@ -1,1 +1,1 @@
-071127
+100214

commit 2cdad105eafa2a56d828d4a37cc3b5f8c305a6fa

ping,ping6: Do not emit audible pings without request.
This bug was introduced by commit 59de58d0f9 ("iputils-s20100214"),
which shuld be a commit just for release without any code changes.

Bug-Debian: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=601147
Signed-off-by: YOSHIFUJI Hideaki <hideaki.yoshifuji@miraclelinux.com>

ping.c
@@ -918,15 +918,14 @@ parse_reply(struct msghdr *msg, int cc, void *addr, struct timeval *tv)
 		return 0;
 	}
 
+	if (options & F_AUDIBLE) {
+		putchar('\a');
+		if (options & F_FLOOD)
+			fflush(stdout);
+	}
 	if (!(options & F_FLOOD)) {
 		pr_options(opts, optlen + sizeof(struct iphdr));
-
-		if (options & F_AUDIBLE)
-			putchar('\a');
 		putchar('\n');
-		fflush(stdout);
-	} else {
-		putchar('\a');
 		fflush(stdout);
 	}
 	return 0;

ping6.c
@@ -1677,13 +1677,13 @@ parse_reply(struct msghdr *msg, int cc, void *addr, struct timeval *tv)
 		pr_icmph(icmph->icmp6_type, icmph->icmp6_code, ntohl(icmph->icmp6_mtu));
 	}
 
-	if (!(options & F_FLOOD)) {
-		if (options & F_AUDIBLE)
-			putchar('\a');
-		putchar('\n');
-		fflush(stdout);
-	} else {
+	if (options & F_AUDIBLE) {
 		putchar('\a');
+		if(options & F_FLOOD)
+			fflush(stdout);
+	}
+	if (!(options & F_FLOOD)) {
+		putchar('\n');
 		fflush(stdout);
 	}
 	return 0;