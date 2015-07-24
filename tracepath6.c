d51ae777112885061c1b29cb5ef2d4b795ef90f5
YOSHIFUJI Hideaki <hideaki.yoshifuji@miraclelinux.com>
2015-07-24 09:56:35 +0900

    tracepath,tracepath6: Fix incorrect 'back' hops.
    Bug-Debian: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=629075
    Bug-Redhat: 1099426
    Signed-off-by: YOSHIFUJI Hideaki <hideaki.yoshifuji@miraclelinux.com>

--- tracepath6.c
+++ tracepath6.c
@@ -261,6 +261,15 @@ restart:
 			printf("(This broken router returned corrupted payload) ");
 	}
 
+	if (rethops >= 0) {
+		if (rethops<=64)
+			rethops = 65-rethops;
+		else if (rethops<=128)
+			rethops = 129-rethops;
+		else
+			rethops = 256-rethops;
+	}
+
 	switch (e->ee_errno) {
 	case ETIMEDOUT:
 		printf("\n");
@@ -286,12 +295,6 @@ restart:
 		     e->ee_type == 3 &&
 		     e->ee_code == 0)) {
 			if (rethops>=0) {
-				if (rethops<=64)
-					rethops = 65-rethops;
-				else if (rethops<=128)
-					rethops = 129-rethops;
-				else
-					rethops = 256-rethops;
 				if (sndhops>=0 && rethops != sndhops)
 					printf("asymm %2d ", rethops);
 				else if (sndhops<0 && rethops != ttl)
