iputils/iputils

commit 40fd68a78417432dd0020cbcf1c8ec01c0714425

traceroute6: Fix udp packet port specification.
Signed-off-by: Noah Meyerhans <noahm@debian.org>

traceroute6.c
@@ -444,7 +444,6 @@ int main(int argc, char *argv[])
 	(void) memset((char *)&whereto, 0, sizeof(whereto));
 
 	to->sin6_family = AF_INET6;
-	to->sin6_port = htons(port);
 
 	if (inet_pton(AF_INET6, *argv, &to->sin6_addr) > 0) {
 		hostname = *argv;
@@ -462,6 +461,7 @@ int main(int argc, char *argv[])
 		}
 
 		memcpy(to, result->ai_addr, sizeof *to);
+		to->sin6_port = htons(port);
 		hostname = result->ai_canonname;
 		freeaddrinfo(result);
 	}

commit 2b6132ef2d45afded759199c4109f9e15401e965

traceroute6: Fix traceroute to IPv6 address
Tracing to an IPv6 address was broken in 40fd68a784 when fixing tracing
to a hostname. It correctly moved the assignment to after the memcpy
into 'to', but that code path is only executed when tracing to a
hostname. The fix just moves the assignment to after the code paths for
filling out 'to' for both IPv6 addresses and hostnames.

traceroute6.c
@@ -462,7 +462,6 @@ int main(int argc, char *argv[])
 		}
 
 		memcpy(to, result->ai_addr, sizeof *to);
-		to->sin6_port = htons(port);
 		resolved_hostname = strdup(result->ai_canonname);
 		if (resolved_hostname == NULL) {
 			(void)fprintf(stderr,
@@ -472,6 +471,9 @@ int main(int argc, char *argv[])
 		hostname = resolved_hostname;
 		freeaddrinfo(result);
 	}
+
+	to->sin6_port = htons(port);
+
 	firsthop = *to;
 	if (*++argv) {
 		datalen = atoi(*argv);