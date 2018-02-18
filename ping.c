75378c651138d2c98c1a2055ac1fd41a196022cc
Claudius Zingerli <gitmail@zeuz.ch>
2018-02-18 20:49:41 +0100

    Ping: Forbid zero timeout
    Setting a timeout of zero (in fact: anything below one) makes ping wait
    infinitely. This commit exits on such values

--- ping.c
+++ ping.c
@@ -459,7 +459,7 @@ main(int argc, char **argv)
 			break;
 		case 'W':
 			lingertime = atoi(optarg);
-			if (lingertime < 0 || lingertime > INT_MAX/1000000) {
+			if (lingertime <= 0 || lingertime > INT_MAX/1000000) {
 				fprintf(stderr, "ping: bad linger time.\n");
 				exit(2);
 			}
