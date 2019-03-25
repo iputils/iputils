iputils/iputils

commit c9aca1b53324bcd1b5a2de5c645813f80eccd016

tracepath: fix musl build, again
Move linux headers after standard headers, that should sort out regression.
Also add a comment that might help keeping includes in good order in future.

Regression-since: 4f0cfdb215c47e04f6b0f73306f8e902cb836b17
Reported-by: Will Dietz <github@wdtz.org>
Reference: https://github.com/iputils/iputils/issues/168
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

tracepath.c
@@ -12,10 +12,6 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <limits.h>
-#include <linux/errqueue.h>
-#include <linux/icmp.h>
-#include <linux/icmpv6.h>
-#include <linux/types.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <resolv.h>
@@ -26,6 +22,15 @@
 #include <sys/time.h>
 #include <sys/uio.h>
 #include <unistd.h>
+
+/*
+ * Keep linux/*.h includes after standard headers.
+ * https://github.com/iputils/iputils/issues/168
+ */
+#include <linux/errqueue.h>
+#include <linux/icmp.h>
+#include <linux/icmpv6.h>
+#include <linux/types.h>
 
 #include "iputils_common.h"
 

commit 0cd6217f9e18dac949fbf88269a7bea9d3ae3b6c

tracepath: fix warning: '/*' within block comment [-Wcomment]
Facepalm caused by c9aca1b53324bcd1b5a2de5c645813f80eccd016.

Signed-off-by: Sami Kerola <kerolasa@iki.fi>

tracepath.c
@@ -24,7 +24,7 @@
 #include <unistd.h>
 
 /*
- * Keep linux/*.h includes after standard headers.
+ * Keep linux/ includes after standard headers.
  * https://github.com/iputils/iputils/issues/168
  */
 #include <linux/errqueue.h>