iputils/iputils

commit 84ca65ca980315c73f929fed8b6f16bbd698c3a0

arping: fix sent vs received packages return value
My earlier signalfd() and timerfd() work really messed up sent vs received
package counter comparison, and related command return value.  Basically
everything always mismatched, and that was expected with corner case of one
package only that did match and caused none-zero return.  Oh my, that is
pretty much as wrong the code could have been.

Reported-by: MarcusRoeckrath
Addresses: https://github.com/iputils/iputils/issues/190
Broken-since: 67e070d08dcbec990e1178360f82b3e2ca4f6d5f
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

arping.c
@@ -792,7 +792,7 @@ static int event_loop(struct run_state *ctl)
 	close(tfd);
 	freeifaddrs(ctl->ifa0);
 	rc |= finish(ctl);
-	rc |= !(ctl->brd_sent != ctl->received);
+	rc |= (ctl->sent != ctl->received);
 	return rc;
 }
 

commit 18f14be80466ddc8fb17a400be82764a779c8dcd

arping: revert partially - fix sent vs received packages return value
Commit 84ca65ca980315c73f929fed8b6f16bbd698c3a0 caused regression.  The
arping -D needs return value evaluation that was the earlier default, in
other cases the new return value should be correct.

Addresses: https://github.com/iputils/iputils/issues/209
See-also: https://github.com/void-linux/void-packages/issues/13304
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

arping.c
@@ -792,7 +792,11 @@ static int event_loop(struct run_state *ctl)
 	close(tfd);
 	freeifaddrs(ctl->ifa0);
 	rc |= finish(ctl);
-	rc |= (ctl->sent != ctl->received);
+	if (ctl->dad && ctl->quit_on_reply)
+		/* Duplicate address detection mode return value */
+		rc |= !(ctl->brd_sent != ctl->received);
+	else
+		rc |= (ctl->sent != ctl->received);
 	return rc;
 }
 