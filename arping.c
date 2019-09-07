iputils/iputils

commit 1df5350bdc952b14901fde356b17b78c2bcd4cff

arping: fix -f quit on first reply regression
When arping runs together with -f 'quit on first reply' and -w <timeout>
'how long to wait for a reply' the command needs to exit if replies are not
received after wait period.  Notice that the exit in case of lost packages
will be 1 signifying failure.  Getting a reply results to 0 exit value.

Addresses: https://bugs.debian.org/935946
Reported-by: Lucas Nussbaum <lucas@debian.org>
Addresses: https://github.com/iputils/iputils/issues/211
Reported-by: Noah Meyerhans <noahm@debian.org>
Broken-since: 67e070d08dcbec990e1178360f82b3e2ca4f6d5f
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

arping.c
@@ -764,7 +764,8 @@ static int event_loop(struct run_state *ctl)
 					continue;
 				}
 				total_expires += exp;
-				if (0 < ctl->count && (uint64_t)ctl->count < total_expires) {
+				if ((0 < ctl->count && (uint64_t)ctl->count < total_expires) ||
+				    (ctl->quit_on_reply && ctl->timeout < total_expires)) {
 					exit_loop = 1;
 					continue;
 				}

commit 68b52adedd5f6af1a3ac027537787d9eaa9cc23b

arping: Fix comparison of different signedness warning
../arping.c:768:45: warning: comparison of integer expressions of different signedness: ‘int’ and ‘uint64_t’ {aka ‘long unsigned int’} [-Wsign-compare]
  768 |         (ctl->quit_on_reply && ctl->timeout < total_expires)) {

Fixes: 1df5350 ("arping: fix -f quit on first reply regression")

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

arping.c
@@ -765,7 +765,7 @@ static int event_loop(struct run_state *ctl)
 				}
 				total_expires += exp;
 				if ((0 < ctl->count && (uint64_t)ctl->count < total_expires) ||
-				    (ctl->quit_on_reply && ctl->timeout < total_expires)) {
+				    (ctl->quit_on_reply && ctl->timeout < (int)total_expires)) {
 					exit_loop = 1;
 					continue;
 				}