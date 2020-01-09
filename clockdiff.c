iputils/iputils

commit 5f5f23fcb3fc800c24692e37ebfb41de5d632dd1

clockdiff: fixup help info
If neither '-o' nor '-o1' option avaliable, using ICMP Timestamp.
run_state::ip_opt_len field controls IP or ICMP timestamp method is used.

clockdiff.c
@@ -451,7 +451,7 @@ static void usage(int exit_status)
 		"\nUsage:\n"
 		"  clockdiff [options] <destination>\n"
 		"\nOptions:\n"
-		"                without -o, use ip timestamp only\n"
+		"                without -o, use icmp timestamp only(see RFC0792, page 16)\n"
 		"  -o            use ip timestamp and icmp echo\n"
 		"  -o1           use three-term ip timestamp and icmp echo\n"
 		"  -T, --time-format <ctime|iso>\n"

commit c5a590c45a554f70b3144e9dc17f93421360ce1e

clockdiff: add missing space to help output
Fixes: 5f5f23fcb3fc800c24692e37ebfb41de5d632dd1
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

clockdiff.c
@@ -451,7 +451,7 @@ static void usage(int exit_status)
 		"\nUsage:\n"
 		"  clockdiff [options] <destination>\n"
 		"\nOptions:\n"
-		"                without -o, use icmp timestamp only(see RFC0792, page 16)\n"
+		"                without -o, use icmp timestamp only (see RFC0792, page 16)\n"
 		"  -o            use ip timestamp and icmp echo\n"
 		"  -o1           use three-term ip timestamp and icmp echo\n"
 		"  -T, --time-format <ctime|iso>\n"