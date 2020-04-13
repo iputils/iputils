93dfb95d48977d151dbe94983e4998959e748aee
Rosen Penev <rosenp@gmail.com>
2020-04-12 21:39:51 -0700

    ping: fix unwanted loss of precision
    Found with bugprone-integer-division
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- ping/ping.c
+++ ping/ping.c
@@ -374,7 +374,7 @@ main(int argc, char **argv)
 			double optval;
 
 			optval = ping_strtod(optarg, _("bad timing interval"));
-			if (isgreater(optval, (double)(INT_MAX / 1000)))
+			if (isgreater(optval, (double)INT_MAX / 1000))
 				error(2, 0, _("bad timing interval: %s"), optarg);
 			rts.interval = (int)(optval * 1000);
 			rts.opt_interval = 1;
@@ -483,7 +483,7 @@ main(int argc, char **argv)
 			double optval;
 
 			optval = ping_strtod(optarg, _("bad linger time"));
-			if (isless(optval, 0.001) || isgreater(optval, (double)(INT_MAX / 1000)))
+			if (isless(optval, 0.001) || isgreater(optval, (double)INT_MAX / 1000))
 				error(2, 0, _("bad linger time: %s"), optarg);
 			/* lingertime will be converted to usec later */
 			rts.lingertime = (int)(optval * 1000);
