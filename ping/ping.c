3b43f90244cd2a06c4395ebd18d665b72e2cef3f
Petr Vorel <pvorel@suse.cz>
2021-01-13 13:26:39 +0100

    ping: Restore -W 0 as infinite timeout
    Fixes: 918e824 ("ping: add support for sub-second timeouts")
    
    References: https://github.com/iputils/iputils/issues/290
    Closes: https://github.com/iputils/iputils/pull/306
    
    Reviewed-by: Noah Meyerhans <noahm@debian.org>
    Reviewed-by: Joerg Vehlow <joerg.vehlow@aox-tech.de>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- ping/ping.c
+++ ping/ping.c
@@ -488,7 +488,7 @@ main(int argc, char **argv)
 			double optval;
 
 			optval = ping_strtod(optarg, _("bad linger time"));
-			if (isless(optval, 0.001) || isgreater(optval, (double)INT_MAX / 1000))
+			if (isless(optval, 0) || isgreater(optval, (double)INT_MAX / 1000))
 				error(2, 0, _("bad linger time: %s"), optarg);
 			/* lingertime will be converted to usec later */
 			rts.lingertime = (int)(optval * 1000);
