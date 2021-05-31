451fdf7b5833e308fe618a37df1952089bf9fac5
Petr Vorel <pvorel@suse.cz>
2021-05-31 19:25:29 +0200

    build.sh: Rewrite to allow running certain step
    i.e. check dependencies / print tools versions, configure,
    build, install.
    
    Required for GitHub Actions CI.
    
    While at it, put code into functions, put info about extra
    meson version check during build.
    
    Link: https://github.com/iputils/iputils/pull/336
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- build.sh
+++ build.sh
@@ -5,57 +5,98 @@ CC="${CC:-gcc}"
 BUILD_DIR="${BUILD_DIR:-builddir}"
 PREFIX="${PREFIX:-$HOME/iputils-install}"
 
-# ninja-build is not detected causes build failing => symlink to ninja
-# needed for CentOS 7 but maybe for others
-if ! which ninja > /dev/null >&2; then
-	if which ninja-build > /dev/null >&2; then
-		ln -sv $(which ninja-build) /usr/local/bin/ninja
-	else
-		echo "ninja binary not found (tried ninja and $NINJA on $PATH)" >&2
-		exit 1
-	fi
-fi
-
-which meson > /dev/null 2>&1 || { echo "meson binary not found" >&2; exit 1; }
-
 BUILD_OPTS="-Dprefix=$PREFIX -DBUILD_RARPD=true -DBUILD_TFTPD=true -DBUILD_TRACEROUTE6=true $EXTRA_BUILD_OPTS"
 [ -z "$EXTRA_BUILD_OPTS" ] && BUILD_OPTS="$BUILD_OPTS -DBUILD_HTML_MANS=true"
 [ -f "meson.cross" ] && BUILD_OPTS="--cross-file $PWD/meson.cross $BUILD_OPTS"
 
-cd `dirname $0`
+# NOTE: meson iself checkes for minimal version
+# see meson_version in meson.build, it fails if not required
+# Meson version is 0.37.1 but project requires >=0.39.
+check_build_dependencies()
+{
+	# ninja-build is not detected causes build failing => symlink to ninja
+	# needed for CentOS 7 but maybe for others
+	if ! which ninja > /dev/null >&2; then
+		if which ninja-build > /dev/null >&2; then
+			ln -sv $(which ninja-build) /usr/local/bin/ninja
+		else
+			echo "ninja binary not found (tried ninja and $NINJA on $PATH)" >&2
+			exit 1
+		fi
+	fi
 
-echo "=== compiler version ==="
-$CC --version
+	which meson > /dev/null 2>&1 || { echo "meson binary not found" >&2; exit 1; }
+}
 
-echo "=== meson version ==="
-meson --version
+print_versions()
+{
+	echo "=== compiler version ==="
+	$CC --version
 
-echo "=== ninja version ==="
-ninja --version
+	echo "=== meson version ==="
+	meson --version
 
-echo "=== build ==="
-echo "Build options: $BUILD_OPTS"
-echo "CFLAGS: $CFLAGS"
+	echo "=== ninja version ==="
+	ninja --version
+}
 
-export CFLAGS
-meson $BUILD_DIR $BUILD_OPTS && \
-make -j$(getconf _NPROCESSORS_ONLN) && make install
-ret=$?
+configure()
+{
+	echo "=== configure ==="
+	echo "Build options: $BUILD_OPTS"
+	echo "CFLAGS: $CFLAGS"
 
-cat << EOF
-============
-END OF BUILD
-============
+	export CFLAGS
+	meson $BUILD_DIR $BUILD_OPTS
+}
 
-EOF
+build()
+{
+	echo "=== build ==="
+	make -j$(getconf _NPROCESSORS_ONLN)
+}
 
-if [ $ret -ne 0 ]; then
+install()
+{
+	echo "=== install ==="
+
+	make install
+}
+
+print_logs()
+{
+	local ret=$1
+	local log
+
+	[ $ret -eq 0 ] && return
+
 	log="$BUILD_DIR/meson-logs/meson-log.txt"
 	if [ -f "$log" ]; then
 		echo "=== START $log ==="
 		cat $log
 		echo "=== END $log ==="
 	fi
+
+	exit $ret
+}
+
+cd `dirname $0`
+
+[ -z "$1" -o "$1" = "dependencies" ] && check_build_dependencies
+
+[ -z "$1" -o "$1" = "info" ] && print_versions
+
+if [ -z "$1" -o "$1" = "configure" ]; then
+	configure
+	print_logs $?
 fi
 
-exit $ret
+if [ -z "$1" -o "$1" = "build" ]; then
+	build
+	print_logs $?
+fi
+
+if [ -z "$1" -o "$1" = "install" ]; then
+	install
+	print_logs $?
+fi
