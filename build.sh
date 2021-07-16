iputils/iputils

commit 4f3ec1da98d4159dd24278d8598da6d25b1543d2

build.sh: Run tests
and run them in GitHub Actions CI.

Closes: https://github.com/iputils/iputils/pull/338
Link: https://github.com/iputils/iputils/issues/243

Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -59,8 +59,15 @@ build()
 install()
 {
 	echo "=== install ==="
-
 	make install
+}
+
+run_tests()
+{
+	echo "=== tests ==="
+	cd $BUILD_DIR
+	meson tests
+	cd -
 }
 
 print_logs()
@@ -98,5 +105,10 @@ fi
 
 if [ -z "$1" -o "$1" = "install" ]; then
 	install
+	print_logs $?
+fi
+
+if [ -z "$1" -o "$1" = "test" ]; then
+	run_tests
 	print_logs $?
 fi

.github/workflows/ci.yml
@@ -106,3 +106,6 @@ jobs:
 
     - name: Install
       run: ./build.sh install
+
+    - name: Run tests
+      run: ./build.sh test

commit a937297157f8a5a50d71fc75cefaf81aa89ce117

build.sh: Fix test command
It fixes error:
ERROR: Neither directory contains a build file meson.build.

Fixes: 4f3ec1d ("build.sh: Run tests")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -66,8 +66,8 @@ run_tests()
 {
 	echo "=== tests ==="
 	cd $BUILD_DIR
-	meson tests
-	cd -
+	meson test
+	cd - > /dev/null
 }
 
 print_logs()