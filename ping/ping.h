iputils/iputils

commit 5e052ada96c3fc457efe66b2fed5c14ed8de6ae3

ping: discard packets with wrong source address
RFC 1122 clearly states that the IP address in the reply MUST match the
one in the request:

	The IP source address in an ICMP Echo Reply MUST be the same
	as the specific-destination address (defined in Section
	3.2.1.3) of the corresponding ICMP Echo Request message.

This is not enforced in iputils neither in IPv4 nor in IPv6:

	$ ping -c4 127.0.0.1
	PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.
	64 bytes from 127.0.0.5: icmp_seq=1 ttl=64 time=0.072 ms
	64 bytes from 127.0.0.5: icmp_seq=2 ttl=64 time=0.101 ms
	64 bytes from 127.0.0.5: icmp_seq=3 ttl=64 time=0.079 ms
	64 bytes from 127.0.0.5: icmp_seq=4 ttl=64 time=0.081 ms

	--- 127.0.0.1 ping statistics ---
	4 packets transmitted, 4 received, 0% packet loss, time 3056ms
	rtt min/avg/max/mdev = 0.072/0.083/0.101/0.010 ms

	$ ping -c4 ::1
	PING ::1(::1) 56 data bytes
	64 bytes from ::5: icmp_seq=1 ttl=64 time=0.029 ms
	64 bytes from ::5: icmp_seq=2 ttl=64 time=0.084 ms
	64 bytes from ::5: icmp_seq=3 ttl=64 time=0.064 ms
	64 bytes from ::5: icmp_seq=4 ttl=64 time=0.083 ms

	--- ::1 ping statistics ---
	4 packets transmitted, 4 received, 0% packet loss, time 3074ms
	rtt min/avg/max/mdev = 0.029/0.065/0.084/0.022 ms

The same issue happens both with raw and ICMP sockets.

Signed-off-by: Matteo Croce <mcroce@redhat.com>

ping/ping6_common.c
@@ -815,6 +815,9 @@ int ping6_parse_reply(struct ping_rts *rts, socket_st *sock,
 	}
 
 	if (icmph->icmp6_type == ICMP6_ECHO_REPLY) {
+		if (!rts->multicast &&
+		    memcmp(&from->sin6_addr.s6_addr, &rts->whereto6.sin6_addr.s6_addr, 16))
+			return 1;
 		if (!is_ours(rts, sock, icmph->icmp6_id))
 			return 1;
 	       if (!contains_pattern_in_payload(rts, (uint8_t *)(icmph + 1)))

ping/ping.c
@@ -1538,6 +1538,9 @@ int ping4_parse_reply(struct ping_rts *rts, struct socket_st *sock,
 	csfailed = in_cksum((unsigned short *)icp, cc, 0);
 
 	if (icp->type == ICMP_ECHOREPLY) {
+		if (!rts->broadcast_pings && !rts->multicast &&
+		    from->sin_addr.s_addr != rts->whereto.sin_addr.s_addr)
+			return 1;
 		if (!is_ours(rts, sock, icp->un.echo.id))
 			return 1;			/* 'Twas not our ECHO */
 		if (!contains_pattern_in_payload(rts, (uint8_t *)(icp + 1)))

commit 78f23a8be6ea3bb30637cd4d4cf91abb917f9b54

ping: Print reply from Subnet-Router anycast address
by detecting Subnet-Router address for 64 bit prefix and suppress
address comparison check.

5e052ad ("ping: discard packets with wrong source address") correctly
hid replies with wrong source address to comply RFC 1122 (Section
3.2.1.3: "The IP source address in an ICMP Echo Reply MUST be the same
as the specific-destination address").

While change in 5e052ad works for broadcast and multicast addresses and
some of anycast addresses, it does not work for (at least) Subnet-Router
anycast address):

    # VETH1_IPV6=fd00:dead:beef:1234::1
    # VPEER1_IPV6=fd00:dead:beef:1234::2
    # ip netns add ns-ipv6
    # ip li add name veth1 type veth peer name vpeer1
    # ip -6 addr add $VETH1_IPV6/64 dev veth1
    # ip li set dev veth1 up
    # ip li set dev vpeer1 netns ns-ipv6
    # ip netns exec ns-ipv6 ip li set dev lo up
    # ip netns exec ns-ipv6 ip -6 addr add $VPEER1_IPV6/64 dev vpeer1
    # ip netns exec ns-ipv6 ip li set vpeer1 up
    # ip netns exec ns-ipv6 ip -6 route add default dev vpeer1 via $VETH1_IPV6
    # sysctl -w net.ipv6.conf.all.forwarding=1

    $ ping -c1 ff02::1 # anycast - all nodes
    PING ff02::1(ff02::1) 56 data bytes
    64 bytes from fe80::9c9c:ffff:fe14:e9d2%vpeer1: icmp_seq=1 ttl=64 time=0.064 ms

    $ ping -c1 ff02::2 # anycast - all routers
    PING ff02::2(ff02::2) 56 data bytes
    64 bytes from fe80::5496:9ff:fef5:8f01%vpeer1: icmp_seq=1 ttl=64 time=0.088 ms

    $ ping -c1 -W5 fd00:dead:beef:1234:: # Subnet-Router anycast
    PING fd00:dead:beef:1234::(fd00:dead:beef:1234::) 56 data bytes

Subnet-Router anycast address works for both busybox ping (without
printing the real source address) and fping:

    $ busybox ping -c1 fd00:dead:beef:1234::
    PING fd00:dead:beef:1234:: (fd00:dead:beef:1234::): 56 data bytes
    64 bytes from fd00:dead:beef:1234::1: seq=0 ttl=64 time=0.122 ms

    $ fping -c1 fd00:dead:beef:1234::
     [<- fd00:dead:beef:1234::1]fd00:dead:beef:1234:: : [0], 64 bytes, 0.096 ms (0.096 avg, 0% loss)

RFC 4291 specifies Subnet-Router anycast address as [1]:

    The Subnet-Router anycast address is predefined.  Its format is as
    follows:
    |                         n bits                 |   128-n bits   |
    +------------------------------------------------+----------------+
    |                   subnet prefix                | 00000000000000 |
    +------------------------------------------------+----------------+

    The "subnet prefix" in an anycast address is the prefix that
    identifies a specific link.  This anycast address is syntactically
    the same as a unicast address for an interface on the link with the
    interface identifier set to zero.

=> to detect Subnet-Router anycast address we need to know prefix, which
we don't know, thus detect it for prefix 64 (the default IPv6 prefix).

Fixes: 5e052ad ("ping: discard packets with wrong source address")

ping/ping.h
@@ -212,6 +212,7 @@ struct ping_rts {
 #endif
 
 	/* Used only in ping6_common.c */
+	int subnet_router_anycast; /* Subnet-Router anycast (RFC 4291) */
 	struct sockaddr_in6 firsthop;
 	unsigned char cmsgbuf[4096];
 	size_t cmsglen;

ping/ping6_common.c
@@ -102,6 +102,7 @@ int ping6_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 	      struct socket_st *sock)
 {
 	int hold, packlen;
+	size_t i;
 	unsigned char *packet;
 	char *target;
 	struct icmp6_filter filter;
@@ -246,6 +247,15 @@ int ping6_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 		}
 		if (rts->pmtudisc < 0)
 			rts->pmtudisc = IPV6_PMTUDISC_DO;
+	}
+
+	/* detect Subnet-Router anycast at least for the default prefix 64 */
+	rts->subnet_router_anycast = 1;
+	for (i = 8; i < sizeof(struct in6_addr); i++) {
+		if (rts->whereto6.sin6_addr.s6_addr[i]) {
+			rts->subnet_router_anycast = 0;
+			break;
+		}
 	}
 
 	if (rts->pmtudisc >= 0) {
@@ -819,7 +829,7 @@ int ping6_parse_reply(struct ping_rts *rts, socket_st *sock,
 	}
 
 	if (icmph->icmp6_type == ICMP6_ECHO_REPLY) {
-		if (!rts->multicast &&
+		if (!rts->multicast && !rts->subnet_router_anycast &&
 		    memcmp(&from->sin6_addr.s6_addr, &rts->whereto6.sin6_addr.s6_addr, 16))
 			return 1;
 		if (!is_ours(rts, sock, icmph->icmp6_id))