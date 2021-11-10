5f3711ef984e4caab0723c48a93cc13d0922fed3
Petr Vorel <pvorel@suse.cz>
2021-11-10 09:59:49 +0100

    CI: Add latest CentOS
    Currently CentOS 8.
    
    That requires to enable powertools repository.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- ci/fedora.sh
+++ ci/fedora.sh
@@ -14,9 +14,16 @@ yum -y install \
 
 yum -y install libidn2-devel docbook5-style-xsl || true
 
-# supported since Centos 7 (Centos 6 don't have python 3.5 meson dependency)
+# supported since CentOS 7 (CentOS 6 don't have python 3.5 meson dependency)
 if [ "$(basename $0)" = "centos.sh" ]; then
+	# CentOS 7: provided by epel
 	yum -y install epel-release
+
+	# CentOS >= 8 provided by PowerTools (but epel still needed)
+	if [ "$DISTRO_VERSION" != 7 ]; then
+		yum -y install dnf-plugins-core
+		yum config-manager --set-enabled powertools
+	fi
 fi
 
 yum -y install meson ninja-build
