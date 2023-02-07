iputils/iputils

commit 37bd60c8b9ec02119af8ded3a0adaccfbfba7701

[iputils] tracepath: Use PMTUDISC_PROBE mode if it exists.
Signed-off-by: John Heffner <jheffner@psc.edu>
Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

tracepath.c
@@ -24,6 +24,10 @@
 #include <sys/uio.h>
 #include <arpa/inet.h>
 
+#ifndef IP_PMTUDISC_PROBE
+#define IP_PMTUDISC_PROBE	3
+#endif
+
 struct hhistory
 {
 	int	hops;
@@ -322,8 +326,10 @@ main(int argc, char **argv)
 	}
 	memcpy(&target.sin_addr, he->h_addr, 4);
 
-	on = IP_PMTUDISC_DO;
-	if (setsockopt(fd, SOL_IP, IP_MTU_DISCOVER, &on, sizeof(on))) {
+	on = IP_PMTUDISC_PROBE;
+	if (setsockopt(fd, SOL_IP, IP_MTU_DISCOVER, &on, sizeof(on)) &&
+	    (on = IP_PMTUDISC_DO,
+	     setsockopt(fd, SOL_IP, IP_MTU_DISCOVER, &on, sizeof(on)))) {
 		perror("IP_MTU_DISCOVER");
 		exit(1);
 	}

tracepath6.c
@@ -30,6 +30,10 @@
 #define SOL_IPV6 IPPROTO_IPV6
 #endif
 
+#ifndef IPV6_PMTUDISC_PROBE
+#define IPV6_PMTUDISC_PROBE	3
+#endif
+
 int overhead = 48;
 int mtu = 128000;
 int hops_to = -1;
@@ -369,8 +373,10 @@ int main(int argc, char **argv)
 		mapped = 1;
 	}
 
-	on = IPV6_PMTUDISC_DO;
-	if (setsockopt(fd, SOL_IPV6, IPV6_MTU_DISCOVER, &on, sizeof(on))) {
+	on = IPV6_PMTUDISC_PROBE;
+	if (setsockopt(fd, SOL_IPV6, IPV6_MTU_DISCOVER, &on, sizeof(on)) &&
+	    (on = IPV6_PMTUDISC_DO,
+	     setsockopt(fd, SOL_IPV6, IPV6_MTU_DISCOVER, &on, sizeof(on)))) {
 		perror("IPV6_MTU_DISCOVER");
 		exit(1);
 	}

commit a75feb0d65b71b0093542b335d189ea445eefb49

tracepath: Restore the MTU probing behavior
Since 81d0f03 (tracepath: fix IP{V6,}_PMTUDISC_DO values), tracepath
shows the PMTU for a target only at the first hop but not the hop where
MTU is reduced, if that data has been cached in kernel FIB by previous
probing attempts.

IP_PMTUDISC_PROBE instructs the kernel to set the Don't Fragment flag
while bypassing PMTU checks. Usage of IP_PMTUDISC_PROBE in tracepath was
introduced in 37bd60c and mentioned in the release note of 20071127
(1d83610) as a feature.

As previously identified in 81d0f03 (appeared in release 20190709), the
definition of these options became incorrect during a copy refactoring in
d3028f1 (in release 20100418). The value for IP_PMTUDISC_PROBE continued
to be used until 81d0f03, when the correct definition of IP_PMTUDISC_DO was
adopted, thus causing a regression.

This commit restores the previous behavior by adopting IP_PMTUDISC_PROBE.

Link: https://github.com/iputils/iputils/pull/448
Fixes: d3028f1 ("tracepath6: Make it more protocol independent.")
Fixes: 81d0f03 ("tracepath: fix IP{V6,}_PMTUDISC_DO values")
Reviewed-by: Petr Vorel <pvorel@suse.cz>
Signed-off-by: Charles Zhang <csalinezh@gmail.com>

iputils_common.h
@@ -55,6 +55,12 @@
 #ifndef IPV6_PMTUDISC_DO
 # define IPV6_PMTUDISC_DO	2
 #endif
+#ifndef IP_PMTUDISC_PROBE
+# define IP_PMTUDISC_PROBE	3
+#endif
+#ifndef IPV6_PMTUDISC_PROBE
+# define IPV6_PMTUDISC_PROBE	3
+#endif
 
 #ifdef HAVE_ERROR_H
 # include <error.h>

tracepath.c
@@ -532,7 +532,7 @@ int main(int argc, char **argv)
 		if (ctl.mtu <= ctl.overhead)
 			goto pktlen_error;
 
-		on = IPV6_PMTUDISC_DO;
+		on = IPV6_PMTUDISC_PROBE;
 		if (setsockopt(ctl.socket_fd, SOL_IPV6, IPV6_MTU_DISCOVER, &on, sizeof(on)) &&
 		    (on = IPV6_PMTUDISC_DO, setsockopt(ctl.socket_fd, SOL_IPV6,
 		     IPV6_MTU_DISCOVER, &on, sizeof(on))))
@@ -557,7 +557,7 @@ int main(int argc, char **argv)
 		if (ctl.mtu <= ctl.overhead)
 			goto pktlen_error;
 
-		on = IP_PMTUDISC_DO;
+		on = IP_PMTUDISC_PROBE;
 		if (setsockopt(ctl.socket_fd, SOL_IP, IP_MTU_DISCOVER, &on, sizeof(on)))
 			error(1, errno, "IP_MTU_DISCOVER");
 		on = 1;