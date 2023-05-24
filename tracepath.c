iputils/iputils

commit 2ca0f7df7f0c954305a1eb3e1d93f736c1793c11

[clang-tidy] fix identical branches
Found with bugprone-branch-clone

Reviewed-by: Petr Vorel <petr.vorel@gmail.com>
Signed-off-by: Rosen Penev <rosenp@gmail.com>

tracepath.c
@@ -321,7 +321,8 @@ static int recverr(struct run_state *const ctl)
 			if (rethops >= 0) {
 				if (sndhops >= 0 && rethops != sndhops)
 					printf(_("asymm %2d "), rethops);
-				else if (sndhops < 0 && rethops != ctl->ttl)
+
+				if (sndhops < 0 && rethops != ctl->ttl)
 					printf(_("asymm %2d "), rethops);
 			}
 			printf("\n");

ninfod/ni_ifaddrs.c
@@ -443,7 +443,6 @@ int ni_ifaddrs(struct ni_ifaddrs **ifap, sa_family_t family)
 							ifamap.broadcast_len = rtapayload;
 							break;
 						case IFA_LABEL:
-							break;
 						case IFA_UNSPEC:
 							break;
 #else

commit 4e3c0949da9213c80b70f576cd2b09804517f97f

tracepath: Merge if clauses
Merge if clauses, because the printed message is the same.
This should have been done in 3337034 ("Initial import of iputils"),
but 2ca0f7d made it even worse when remove else from else if
(clang-tidy was obviously wrong :)).

Fixes: 2ca0f7d ("[clang-tidy] fix identical branches")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

tracepath.c
@@ -321,10 +321,8 @@ static int recverr(struct run_state *const ctl)
 		     e->ee_type == ICMPV6_TIME_EXCEED &&
 		     e->ee_code == ICMPV6_EXC_HOPLIMIT)) {
 			if (rethops >= 0) {
-				if (sndhops >= 0 && rethops != sndhops)
-					printf(_("asymm %2d "), rethops);
-
-				if (sndhops < 0 && rethops != ctl->ttl)
+				if ((sndhops >= 0 && rethops != sndhops) ||
+					(sndhops < 0 && rethops != ctl->ttl))
 					printf(_("asymm %2d "), rethops);
 			}
 			printf("\n");