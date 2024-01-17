iputils/iputils

commit 2a63b94313352ed62f47adbc1a0d33bfea7ca188

ping: Fix overflow on negative -i
Restore back check for -i <= 0.

Fixes: 918e824 ("ping: add support for sub-second timeouts")
Closes: https://github.com/iputils/iputils/issues/465
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ping/ping.c
@@ -436,7 +436,7 @@ main(int argc, char **argv)
 			double optval;
 
 			optval = ping_strtod(optarg, _("bad timing interval"));
-			if (isgreater(optval, (double)INT_MAX / 1000))
+			if (islessequal(optval, 0) || isgreater(optval, (double)INT_MAX / 1000))
 				error(2, 0, _("bad timing interval: %s"), optarg);
 			rts.interval = (int)(optval * 1000);
 			rts.opt_interval = 1;

commit 7a514946c85ebfb3d12428ed1991262ac3dfc724

ping: Restore -i0
-i 0 has been supported for a long time, restore it back.
There is no point to force user to set very minimal value to emulate 0.

Found by LTP tests (nft01.sh, iptables01.sh) which use it.

Fixes: 2a63b94 ("ping: Fix overflow on negative -i")
Closes: https://github.com/iputils/iputils/pull/519
Reviewed-by: David Heidelberg <david@ixit.cz>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ping/ping.c
@@ -441,7 +441,7 @@ main(int argc, char **argv)
 			double optval;
 
 			optval = ping_strtod(optarg, _("bad timing interval"));
-			if (islessequal(optval, 0) || isgreater(optval, (double)INT_MAX / 1000))
+			if (isless(optval, 0) || isgreater(optval, (double)INT_MAX / 1000))
 				error(2, 0, _("bad timing interval: %s"), optarg);
 			rts.interval = (int)(optval * 1000);
 			rts.opt_interval = 1;