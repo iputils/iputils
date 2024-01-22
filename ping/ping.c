iputils/iputils

commit 08f0e1762d8aa4e8d99fc1670716280c62f9cb27

ping: Set mark (SO_MARK) on probe socket
If the bind (source) IP is is not explicitly specified with "-I" then
ping creates a "probe" socket to determine the source IP.  However, if a
mark is configured with "-m", the current code does not apply this mark
to the "probe" socket.  If policy routing is configured such that the
mark affects source IP selection, then the wrong source IP will be used.

In addition, if policy routing is configured such that the destination
will be unreachable unless the mark is set, then the probe socket will
fail and ping will exit with an error.

Therefore set the mark on the probe socket in addition to the actual
ping socket.

Testing emulates the real scenario requires to disable rp_filter (source
validation), use 2 netns to avoid mangling sysctl rp_filter setup:

    IPUTILS_NS1="ip netns exec iputils_ns1"
    IPUTILS_NS2="ip netns exec iputils_ns2"
    ip netns add iputils_ns1
    ip netns add iputils_ns2
    ip link add name iputils_veth1 type veth peer name iputils_veth2
    ip link set dev iputils_veth1 netns iputils_ns1
    ip link set dev iputils_veth2 netns iputils_ns2
    $IPUTILS_NS1 ip link set dev iputils_veth1 up
    $IPUTILS_NS1 ip addr add 10.0.0.2/24 dev iputils_veth1
    $IPUTILS_NS1 ip rule add priority 101 fwmark 0x1/0x1 table 1
    $IPUTILS_NS1 ip route add table 1 10.0.1.0/24 dev iputils_veth1 proto kernel scope link src 10.0.0.2
    $IPUTILS_NS1 sysctl -w net.ipv4.conf.all.rp_filter=0
    $IPUTILS_NS1 sysctl -w net.ipv4.conf.iputils_veth1.rp_filter=0
    $IPUTILS_NS2 ip link set dev iputils_veth2 up
    $IPUTILS_NS2 ip addr add 10.0.0.1/24 dev iputils_veth2
    $IPUTILS_NS2 ip addr add 10.0.1.1/24 dev iputils_veth2

    $IPUTILS_NS1 iputils.patched/builddir/ping/ping 10.0.1.1 -c1 -m1

    ip netns del iputils_ns1
    ip netns del iputils_ns2

Closes: https://github.com/iputils/iputils/pull/340

Reviewed-by: Petr Vorel <pvorel@suse.cz>
Tested-by: Petr Vorel <pvorel@suse.cz>

ping/ping.c
@@ -715,6 +715,9 @@ int ping4_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 		    setsockopt(probe_fd, IPPROTO_IP, IP_TOS, (char *)&rts->settos, sizeof(int)) < 0)
 			error(0, errno, _("warning: QOS sockopts"));
 
+		if (rts->opt_mark)
+			sock_setmark(rts->mark, probe_fd);
+
 		dst.sin_port = htons(1025);
 		if (rts->nroute)
 			dst.sin_addr.s_addr = rts->route[0];

ping/ping.h
@@ -380,6 +380,7 @@ char *pr_addr(struct ping_rts *rts, void *sa, socklen_t salen);
 int is_ours(struct ping_rts *rts, socket_st *sock, uint16_t id);
 extern int pinger(struct ping_rts *rts, ping_func_set_st *fset, socket_st *sock);
 extern void sock_setbufs(struct ping_rts *rts, socket_st *, int alloc);
+extern void sock_setmark(unsigned int mark, int fd);
 extern void setup(struct ping_rts *rts, socket_st *);
 extern int main_loop(struct ping_rts *rts, ping_func_set_st *fset, socket_st*,
 		     uint8_t *packet, int packlen);

ping/ping6_common.c
@@ -183,6 +183,9 @@ int ping6_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 		    !IN6_IS_ADDR_MC_LINKLOCAL(&rts->firsthop.sin6_addr))
 			rts->firsthop.sin6_family = AF_INET6;
 
+		if (rts->opt_mark)
+			sock_setmark(rts->mark, probe_fd);
+
 		rts->firsthop.sin6_port = htons(1025);
 		if (connect(probe_fd, (struct sockaddr *)&rts->firsthop, sizeof(rts->firsthop)) == -1) {
 			if ((errno == EHOSTUNREACH || errno == ENETUNREACH) && ai->ai_next) {

ping/ping_common.c
@@ -446,6 +446,25 @@ void sock_setbufs(struct ping_rts *rts, socket_st *sock, int alloc)
 	}
 }
 
+void sock_setmark(unsigned int mark, int fd)
+{
+#ifdef SO_MARK
+	int ret;
+	int errno_save;
+
+	enable_capability_admin();
+	ret = setsockopt(fd, SOL_SOCKET, SO_MARK, &mark, sizeof(mark));
+	errno_save = errno;
+	disable_capability_admin();
+
+	/* Do not exit, old kernels do not support mark. */
+	if (ret == -1)
+		error(0, errno_save, _("WARNING: failed to set mark: %d"), mark);
+#else
+		error(0, errno_save, _("WARNING: SO_MARK not supported"));
+#endif
+}
+
 /* Protocol independent setup and parameter checks. */
 
 void setup(struct ping_rts *rts, socket_st *sock)
@@ -476,22 +495,9 @@ void setup(struct ping_rts *rts, socket_st *sock)
 			error(0, 0, _("Warning: no SO_TIMESTAMP support, falling back to SIOCGSTAMP"));
 	}
 #endif
-#ifdef SO_MARK
-	if (rts->opt_mark) {
-		int ret;
-		int errno_save;
 
-		enable_capability_admin();
-		ret = setsockopt(sock->fd, SOL_SOCKET, SO_MARK, &rts->mark, sizeof(rts->mark));
-		errno_save = errno;
-		disable_capability_admin();
-
-		if (ret == -1) {
-			/* Do not exit, old kernels do not support mark. */
-			error(0, errno_save, _("Warning: Failed to set mark: %d"), rts->mark);
-		}
-	}
-#endif
+	if (rts->opt_mark)
+		sock_setmark(rts->mark, sock->fd);
 
 	/* Set some SNDTIMEO to prevent blocking forever
 	 * on sends, when device is too slow or stalls. Just put limit

commit 1c081524ee463842631c708d060119ebc7ca699f

ping: Refactor socket mark failure handling
Ping warned 2x about setsockopt SO_MARK failure, because since 08f0e17
is called also for probe socket (before it was only on real socket):

    $ ping -m 2 ::1
    ping: WARNING: failed to set mark: 2: Operation not permitted
    PING ::1 (::1) 56 data bytes
    ping: WARNING: failed to set mark: 2: Operation not permitted
    64 bytes from ::1: icmp_seq=1 ttl=64 time=0.039 ms

Therefore pass rts struct to:
1) Disable SO_MARK call on real socket when it failed on probe socket,
therefore only single warning is printed.
2) Print hint about possible missing cap_net_admin+p capability

    $ ./builddir/ping/ping -m 2 ::1
    ./builddir/ping/ping: WARNING: failed to set mark: 2: Operation not permitted
    ./builddir/ping/ping: => missing cap_net_admin+p capability?
    PING ::1 (::1) 56 data bytes
    64 bytes from ::1: icmp_seq=1 ttl=64 time=0.053 ms

Fixes: 08f0e17 ("ping: Set mark (SO_MARK) on probe socket")
Fixes: https://github.com/iputils/iputils/issues/515
Reviewed-by: Matthias Gerstner <matthias.gerstner@suse.com>
Reviewed-by: Paul Donohue <git@PaulSD.com>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ping/ping.c
@@ -828,8 +828,7 @@ int ping4_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 		    setsockopt(probe_fd, IPPROTO_IP, IP_TOS, (char *)&rts->settos, sizeof(int)) < 0)
 			error(0, errno, _("warning: QOS sockopts"));
 
-		if (rts->opt_mark)
-			sock_setmark(rts->mark, probe_fd);
+		sock_setmark(rts, probe_fd);
 
 		dst.sin_port = htons(1025);
 		if (rts->nroute)

ping/ping.h
@@ -395,7 +395,7 @@ char *str_interval(int interval);
 int is_ours(struct ping_rts *rts, socket_st *sock, uint16_t id);
 extern int pinger(struct ping_rts *rts, ping_func_set_st *fset, socket_st *sock);
 extern void sock_setbufs(struct ping_rts *rts, socket_st *, int alloc);
-extern void sock_setmark(unsigned int mark, int fd);
+extern void sock_setmark(struct ping_rts *rts, int fd);
 extern void setup(struct ping_rts *rts, socket_st *);
 extern int main_loop(struct ping_rts *rts, ping_func_set_st *fset, socket_st*,
 		     uint8_t *packet, int packlen);

ping/ping6_common.c
@@ -190,8 +190,7 @@ int ping6_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 		    !IN6_IS_ADDR_MC_LINKLOCAL(&rts->firsthop.sin6_addr))
 			rts->firsthop.sin6_family = AF_INET6;
 
-		if (rts->opt_mark)
-			sock_setmark(rts->mark, probe_fd);
+		sock_setmark(rts, probe_fd);
 
 		rts->firsthop.sin6_port = htons(1025);
 		if (connect(probe_fd, (struct sockaddr *)&rts->firsthop, sizeof(rts->firsthop)) == -1) {

ping/ping_common.c
@@ -455,20 +455,28 @@ void sock_setbufs(struct ping_rts *rts, socket_st *sock, int alloc)
 	}
 }
 
-void sock_setmark(unsigned int mark, int fd)
+void sock_setmark(struct ping_rts *rts, int fd)
 {
 #ifdef SO_MARK
 	int ret;
 	int errno_save;
 
+	if (!rts->opt_mark)
+		return;
+
 	enable_capability_admin();
-	ret = setsockopt(fd, SOL_SOCKET, SO_MARK, &mark, sizeof(mark));
+	ret = setsockopt(fd, SOL_SOCKET, SO_MARK, &(rts->mark), sizeof(rts->mark));
 	errno_save = errno;
 	disable_capability_admin();
 
-	/* Do not exit, old kernels do not support mark. */
-	if (ret == -1)
-		error(0, errno_save, _("WARNING: failed to set mark: %u"), mark);
+	if (ret == -1) {
+		error(0, errno_save, _("WARNING: failed to set mark: %u"), rts->mark);
+
+		if (errno_save == EPERM)
+			error(0, 0, _("=> missing cap_net_admin+p or cap_net_raw+p (since Linux 5.17) capability?"));
+
+		rts->opt_mark = 0;
+	}
 #else
 		error(0, errno_save, _("WARNING: SO_MARK not supported"));
 #endif
@@ -506,8 +514,7 @@ void setup(struct ping_rts *rts, socket_st *sock)
 	}
 #endif
 
-	if (rts->opt_mark)
-		sock_setmark(rts->mark, sock->fd);
+	sock_setmark(rts, sock->fd);
 
 	/* Set some SNDTIMEO to prevent blocking forever
 	 * on sends, when device is too slow or stalls. Just put limit