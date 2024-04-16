iputils/iputils

commit 26c024783a7244d36fa00e0a24df4f072a88af69

build.sh: set -eux to quit early
-e: to quit early (we have run for checking error code, but it will be
useful to quit on code which does not use it)
-u: check for use of unset variables
-x: print executed command

Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
-# Copyright (c) 2019-2021 Petr Vorel <pvorel@suse.cz>
+#!/bin/sh -eux
+# Copyright (c) 2019-2024 Petr Vorel <pvorel@suse.cz>
 
 CFLAGS="${CFLAGS:--Wformat -Werror=format-security -Werror=implicit-function-declaration -Werror=return-type -fno-common}"
 CC="${CC:-gcc}"
 BUILD_DIR="${BUILD_DIR:-builddir}"
 PREFIX="${PREFIX:-$HOME/iputils-install}"
 
+[ -z "${EXTRA_BUILD_OPTS:-}" ] && EXTRA_BUILD_OPTS="-DBUILD_HTML_MANS=true"
 BUILD_OPTS="-Dprefix=$PREFIX $EXTRA_BUILD_OPTS"
-[ -z "$EXTRA_BUILD_OPTS" ] && BUILD_OPTS="$BUILD_OPTS -DBUILD_HTML_MANS=true"
 [ -f "meson.cross" ] && BUILD_OPTS="--cross-file $PWD/meson.cross $BUILD_OPTS"
 
 # NOTE: meson iself checkes for minimal version
@@ -110,8 +110,8 @@ print_log()
 cd `dirname $0`
 
 cmd=
-case "$1" in
-	dependencies|info|configure|build|build-log|install|install-log|test|test-log|"") cmd="$1";;
+case "${1:-}" in
+	dependencies|info|configure|build|build-log|install|install-log|test|test-log|"") cmd="${1:-}";;
 	*) echo "ERROR: wrong command '$1'" >&2; exit 1;;
 esac
 

commit 09cd18dab73e687455bbf4987b4b3357d026a808

build.sh: Remove run() function
Code is not used any more due set -e in 26c0247.

Fixes: 26c0247 ("build.sh: set -eux to quit early")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -41,19 +41,6 @@ print_versions()
 	ninja --version
 }
 
-run()
-{
-	local ret
-
-	eval "$@"
-	ret=$?
-
-	if [ $ret -ne 0 ]; then
-		echo "ERROR: '$@' failed, exit code: $ret" >&2
-		exit $ret
-	fi
-}
-
 configure()
 {
 	echo "=== configure ==="
@@ -62,19 +49,19 @@ configure()
 
 	export CFLAGS
 
-	run "meson setup $BUILD_DIR $BUILD_OPTS"
+	meson setup $BUILD_DIR $BUILD_OPTS
 }
 
 build()
 {
 	echo "=== build ==="
-	run "make -j$(getconf _NPROCESSORS_ONLN)"
+	make -j$(getconf _NPROCESSORS_ONLN)
 }
 
 install()
 {
 	echo "=== install ==="
-	run "make install"
+	make install
 }
 
 dist()
@@ -83,7 +70,7 @@ dist()
 	local f
 
 	echo "=== dist ($formats) ==="
-	run "meson dist -C $BUILD_DIR --formats $formats"
+	meson dist -C $BUILD_DIR --formats $formats
 
 	for f in $(echo "$formats" | sed 's/,/ /g'); do
 		f=$(echo "$f" | sed 's/\(.*\)tar/tar.\1/')