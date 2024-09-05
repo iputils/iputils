iputils/iputils

commit 2e8b3b27e8e779d1e18a4dcbf9aa28d4e31a5b4a

build.sh: Add support for meson dist
For testing a release build.

Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -77,6 +77,22 @@ install()
 	run "make install"
 }
 
+dist()
+{
+	local formats="xztar,gztar,zip"
+	local f
+
+	echo "=== dist ($formats) ==="
+	run "meson dist -C $BUILD_DIR --formats $formats"
+
+	for f in $(echo "$formats" | sed 's/,/ /g'); do
+		f=$(echo "$f" | sed 's/\(.*\)tar/tar.\1/')
+		f=$BUILD_DIR/meson-dist/iputils-20240117.$f
+		ls -lah $f
+		file $f | grep -E '(compressed|archive) data'
+	done
+}
+
 run_tests()
 {
 	local ret
@@ -111,7 +127,7 @@ cd `dirname $0`
 
 cmd=
 case "${1:-}" in
-	dependencies|info|configure|build|build-log|install|install-log|test|test-log|"") cmd="${1:-}";;
+	build|build-log|configure|dependencies|dist|info|install|install-log|test|test-log|"") cmd="${1:-}";;
 	*) echo "ERROR: wrong command '$1'" >&2; exit 1;;
 esac
 
@@ -137,6 +153,10 @@ fi
 
 if [ -z "$cmd" -o "$cmd" = "install" ]; then
 	install
+fi
+
+if [ -z "$cmd" -o "$cmd" = "dist" ]; then
+	dist
 fi
 
 if [ "$cmd" = "install-log" ]; then

commit 6d1f6e0f822db602c9569b2a668c22421bd96c2b

build.sh: Fix testing release build
Fixes: 2e8b3b2 ("build.sh: Add support for meson dist")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -133,6 +133,7 @@ install()
 dist()
 {
 	local formats="xztar,gztar,zip"
+	local tag="$(date +%Y%m%d)"
 	local f
 
 	echo "=== dist ($formats) ==="
@@ -140,7 +141,7 @@ dist()
 
 	for f in $(echo "$formats" | sed 's/,/ /g'); do
 		f=$(echo "$f" | sed 's/\(.*\)tar/tar.\1/')
-		f=$BUILD_DIR/meson-dist/iputils-20240117.$f
+		f=$BUILD_DIR/meson-dist/iputils-$tag.$f
 		ls -lah $f
 		file $f | grep -E '(compressed|archive) data'
 	done