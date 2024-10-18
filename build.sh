iputils/iputils

commit 2e8b3b27e8e779d1e18a4dcbf9aa28d4e31a5b4a

build.sh: Add support for meson dist
For testing a release build.

Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -77,6 +77,22 @@ install()
 	run "make install"
 }
 
+dist()
+{
+	local formats="xztar,gztar,zip"
+	local f
+
+	echo "=== dist ($formats) ==="
+	run "meson dist -C $BUILD_DIR --formats $formats"
+
+	for f in $(echo "$formats" | sed 's/,/ /g'); do
+		f=$(echo "$f" | sed 's/\(.*\)tar/tar.\1/')
+		f=$BUILD_DIR/meson-dist/iputils-20240117.$f
+		ls -lah $f
+		file $f | grep -E '(compressed|archive) data'
+	done
+}
+
 run_tests()
 {
 	local ret
@@ -111,7 +127,7 @@ cd `dirname $0`
 
 cmd=
 case "${1:-}" in
-	dependencies|info|configure|build|build-log|install|install-log|test|test-log|"") cmd="${1:-}";;
+	build|build-log|configure|dependencies|dist|info|install|install-log|test|test-log|"") cmd="${1:-}";;
 	*) echo "ERROR: wrong command '$1'" >&2; exit 1;;
 esac
 
@@ -137,6 +153,10 @@ fi
 
 if [ -z "$cmd" -o "$cmd" = "install" ]; then
 	install
+fi
+
+if [ -z "$cmd" -o "$cmd" = "dist" ]; then
+	dist
 fi
 
 if [ "$cmd" = "install-log" ]; then

commit b912d64c2a4ed9465e6bd7fa144d2353b317aa09

build.sh: Fix tarball name
'meson dist' produces tarball name based on the version in meson.build,
not on the date of run.

Using meson tools is safer than parsing meson.build with sed, but it
requires to add install jq.

For getting the latest version cannot be used much simpler 'git describe
--abbrev=0' (the latest tag), because fork might have fewer tags or even
no tag.

Fixes: 2e8b3b2 ("build.sh: Add support for meson dist")
Fixes: 6d1f6e0 ("build.sh: Fix testing release build")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -133,7 +133,7 @@ install()
 dist()
 {
 	local formats="xztar,gztar,zip"
-	local tag="$(date +%Y%m%d)"
+	local tag="$(meson introspect $BUILD_DIR --projectinfo | jq -r '.version')"
 	local f
 
 	echo "=== dist ($formats) ==="

ci/alpine.sh
@@ -13,8 +13,9 @@ apk add \
 	file \
 	gcc \
 	git \
-	iproute2 \
 	gettext-dev \
+	iproute2 \
+	jq \
 	libcap-dev \
 	libxslt \
 	make \

ci/debian.sh
@@ -23,6 +23,7 @@ apt install -y --no-install-recommends \
 	gettext \
 	git \
 	iproute2 \
+	jq \
 	libcap-dev \
 	libidn2-0-dev \
 	make \

ci/fedora.sh
@@ -11,6 +11,7 @@ yum -y install \
 	gettext \
 	git \
 	iproute \
+	jq \
 	libcap-devel \
 	libxslt \
 	make \

ci/opensuse.sh
@@ -12,6 +12,7 @@ zypper --non-interactive install --no-recommends \
 	gettext-tools \
 	git \
 	iproute2 \
+	jq \
 	libcap-devel \
 	libcap-progs \
 	libidn2-devel \