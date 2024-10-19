ae0e0c24b184eb25fafcb9946e1d3e0436399d9a
Lars Uffmann <coding23@uffmann.name>
2024-10-19 23:52:13 +0200

    ping ipv4 addresses masked as ipv6 after resolving hostname etc
    Signed-off-by: Lars Uffmann <coding23@uffmann.name>

--- ping/ping.c
+++ ping/ping.c
@@ -93,6 +93,9 @@ ping_func_set_st ping4_func_set = {
 
 #define CASE_TYPE(x) case x: return #x;
 
+const unsigned char IPV4_MASK_IN_IPV6[ 12 ] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff };
+#define SA6_IS_ADDR_V4MAPPED( addr ) ( memcmp( &( (struct sockaddr_in6 * ) addr)->sin6_addr, IPV4_MASK_IN_IPV6, 12 ) == 0 )
+
 static char *str_family(int family)
 {
 	switch (family) {
@@ -353,7 +356,6 @@ main(int argc, char **argv)
 		.ni.subject_type = -1,
 	};
 	unsigned char buf[sizeof(struct in6_addr)];
-	struct in6_addr a6;
 
 	/* FIXME: global_rts will be removed in future */
 	global_rts = &rts;
@@ -621,14 +623,6 @@ main(int argc, char **argv)
 		hints.ai_socktype = SOCK_RAW;
 	}
 
-	if (inet_pton(AF_INET6, target, &a6) && IN6_IS_ADDR_V4MAPPED(&a6)) {
-			target = strrchr(target, ':') + 1;
-			hints.ai_family = AF_INET;
-
-			if (rts.opt_verbose)
-				error(0, 0, _("IPv4-Mapped-in-IPv6 address, using IPv4 %s"), target);
-	}
-
 	if (hints.ai_family != AF_INET6) {
 		create_socket(&rts, &sock4, AF_INET, hints.ai_socktype, IPPROTO_ICMP,
 			      hints.ai_family == AF_INET);
@@ -727,7 +721,33 @@ main(int argc, char **argv)
 			ret_val = ping4_run(&rts, argc, argv, ai, &sock4);
 			break;
 		case AF_INET6:
-			ret_val = ping6_run(&rts, argc, argv, ai, &sock6);
+			if( SA6_IS_ADDR_V4MAPPED( ai->ai_addr ) ) { // if ipv6 is actually a masked ipv4 address
+				if (rts.opt_verbose)
+					error(0, 0, _("IPv4-Mapped-in-IPv6 address, using IPv4 %s"), target);
+
+				// compose an ipv4 addressinfo record valid *only* for a call to ping4_run:
+				struct sockaddr_in sa4;
+				memset( &sa4, 0, sizeof( struct sockaddr_in ) );   // optional: zero out this sockaddr_in
+				sa4.sin_family = AF_INET;                          // ipv4 family
+				sa4.sin_port = 0;                                  // port doesn't matter in context
+				const int IPV4_OFFSET_IN_IPV6 = sizeof( struct in6_addr ) - sizeof( struct in_addr ); // 12
+				// copy ipv4 part of ipv6 into ipv4 record:
+				memcpy( &sa4.sin_addr, ( ( char * ) &( (struct sockaddr_in6 * ) ai->ai_addr)->sin6_addr ) + IPV4_OFFSET_IN_IPV6, sizeof( struct in_addr ) );
+
+				struct addrinfo ai4;
+				ai4.ai_flags = 0;                // TBD by someone smarter than me: does this matter for ping?
+				ai4.ai_family = AF_INET;         // ipv4
+				ai4.ai_socktype = 0;             // SOCK_UNSPEC: TBD by someone smarter than me: does this matter for ping?
+				ai4.ai_protocol = IPPROTO_ICMP;  // TBD by someone smarter than me: is IPPROTO_ICMP == 1 okay? seems to default to IPPROTO_TCP == 6
+				ai4.ai_addrlen = sizeof( struct sockaddr_in );   // ipv4 socket address
+				ai4.ai_addr = ( struct sockaddr * ) &sa4;        // as initialized above
+				ai4.ai_canonname = NULL;         // unused
+				ai4.ai_next = NULL;              // unused (should not matter for ping4_run, otherwise could be set to result->ai_next
+
+				ret_val = ping4_run(&rts, argc, argv, &ai4, &sock4);
+			}
+			else // actual ipv6
+				ret_val = ping6_run(&rts, argc, argv, ai, &sock6);
 			break;
 		default:
 			error(2, 0, _("unknown protocol family: %d"), ai->ai_family);
