iputils/iputils

commit 22debcf51df21ff1fd91c6200192f40c81dfc101

ci: Add 32bit build
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ci/debian.i386.sh
@@ -1,0 +1,15 @@
+#!/bin/sh -eux
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2024 Petr Vorel <pvorel@suse.cz>
+
+dpkg --add-architecture i386
+apt update
+
+apt install -y --no-install-recommends \
+	gcc-multilib \
+	libcap-dev:i386 \
+	libc6-dev-i386 \
+	libc6:i386 \
+	libidn2-0-dev:i386 \
+	libssl-dev:i386 \
+	pkg-config:i386

.github/workflows/ci.yml
@@ -12,6 +12,13 @@ jobs:
       fail-fast: false
       matrix:
         include:
+          # 32bit build
+          - container: "debian:stable"
+            env:
+              CC: gcc
+              VARIANT: i386
+              BUILD_32: 1
+
           # cross compilation builds
           - container: "debian:testing"
             env:

commit 3bb2d7302618d8c3708d3c93adcd90f342f5b6dc

ci: debian: Remove libssl-dev
iputils does not use openSSL since 214ed83.

Fixes: 22debcf ("ci: Add 32bit build")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ci/debian.i386.sh
@@ -11,5 +11,4 @@ apt install -y --no-install-recommends \
 	libc6-dev-i386 \
 	libc6:i386 \
 	libidn2-0-dev:i386 \
-	libssl-dev:i386 \
 	pkg-config:i386