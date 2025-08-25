iputils/iputils

commit 1319d603d0357e9ef00ac2dd0364d88e1b278497

build.sh: Fix 32bit pattern for Debian 13 (trixie)
'file' 5.46 in Debian 13 (trixie) started to print i386 instead of 80386.
Fix pattern (allow both versions).

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -eux
-# Copyright (c) 2019-2024 Petr Vorel <pvorel@suse.cz>
+# Copyright (c) 2019-2025 Petr Vorel <pvorel@suse.cz>
 
 CFLAGS="${CFLAGS:--Wformat -Werror=format-security -Werror=implicit-function-declaration -Werror=return-type -fno-common}"
 CC="${CC:-gcc}"
@@ -112,7 +112,7 @@ check_binaries()
 
 	if [ "${BUILD_32:-}" ]; then
 		bits=32
-		arch='80386'
+		arch='(80386|i386)'
 	fi
 
 	for i in $BINARIES; do
@@ -122,6 +122,7 @@ check_binaries()
 			continue
 		fi
 		[ -x "$BUILD_DIR/$i" ]
+		file "$BUILD_DIR/$i" # debug
 		file "$BUILD_DIR/$i" | grep -E "$i.*${bits}-bit .*(executable|shared object).*$arch.*dynamically linked"
 	done
 }

commit 68f344c14fa6013de1a52a36be54a27b01d999d1

build.sh: Cleanup debug output
Fixes: 1319d60 ("build.sh: Fix 32bit pattern for Debian 13 (trixie)")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -122,7 +122,6 @@ check_binaries()
 			continue
 		fi
 		[ -x "$BUILD_DIR/$i" ]
-		file "$BUILD_DIR/$i" # debug
 		file "$BUILD_DIR/$i" | grep -E "$i.*${bits}-bit .*(executable|shared object).*$arch.*dynamically linked"
 	done
 }